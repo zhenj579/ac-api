c66c0a87c37aec5856927addc8ceafcad865268e
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-11-27 20:59:03 -0500

    changed favoritesongresult to property

--- AnimalCrossingApi/Models/Response.cs
+++ AnimalCrossingApi/Models/Response.cs
@@ -4,7 +4,7 @@
     {
         public int statusCode { get; set; }
         public string statusDescription { get; set; }
-        public List<FavoriteSongs> favoriteSongsResult = new();
+        public List<FavoriteSongs> favoriteSongsResult { get; set; } = new();
         public List<Songs> songsResult { get; set; } = new();
         public List<Villager> villagersResult { get; set; } = new();
 
